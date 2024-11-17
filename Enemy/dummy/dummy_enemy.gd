c89db6f7c9d6276c0fb89ea82ec0c41fbb00525b
Ferencz Hollander <f.m.t.hollander@student.tudelft.nl>
2024-11-17 18:07:24 +0100

    Enemy loopt

---
+++ Enemy/dummy/dummy_enemy.gd
@@ -0,0 +1,1 @@
+extends "res://Enemy/enemy.gd"
--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,23 +1,29 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const direction = -0.5
-const damage = 1
+@export var SPEED = 100.0
+@export var MOVE_DISTANCE = 100
+var direction = 1
+var damage = 1
+var start_pos: int
 
+func _ready() -> void:
+	start_pos = position.x
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
-		
-	velocity.x = direction * SPEED
-
+		velocity += get_gravity() * delta 
+	else:
+		velocity.x = SPEED*direction
+		if (position.x - start_pos) < 0:
+			direction = 1
+			position.x = start_pos
+		elif (position.x - start_pos) > MOVE_DISTANCE:
+			direction = -1
+			position.x = MOVE_DISTANCE+start_pos
+			
 	move_and_slide()
-	
-	
 func getDamage() -> int:
 	return damage
 	
