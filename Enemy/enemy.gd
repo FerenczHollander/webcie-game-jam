509a96c1661fc86d28a4fa8d2ef87cec1a48eaa5
Daan <daanvanderaar@hotmail.com>
2024-11-17 16:10:36 +0100

    woow

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const direction = -1
+const direction = -0.5
 
 
 func _physics_process(delta: float) -> void:
--- Player/PlayerScript.gd
+++ Player/PlayerScript.gd
@@ -11,7 +11,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not JUMPING:
 		JUMPING = true;
@@ -31,4 +30,9 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
